f92d5f4604ed53ca1e40168a6b1b5f723e5f5891
DamianoL <damianolunardo@hotmail.it>
2023-10-10 16:40:46 +0200

    Add milestone 2-3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// array delle immagini del carosello
+// Array delle immagini del carosello
 const images = [
     '01.webp',
     '02.webp',
@@ -7,9 +7,46 @@ const images = [
     '05.webp'
 ];
 
-// seleziona l'elemento carousel
+// Seleziona gli elementi del carosello ed i pulsanti di navigazione
 const carouselSlide = document.querySelector('.carousel-slide');
-const prevButton = document.querySelector('prevButton');
-const nextButton = document.querySelector('nextButton');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
 
+
+// Inizializza l'indice
 let currentIndex = 0;
+
+// Ora devo mostrare l'immagine corrente. L'indice viene moltiplicato per -100% per determinare quanto spostare l'elemento.
+function showSlide(index) {
+    carouselSlide.style.transform = `translateX(-${index * 100}%)`;
+};
+
+// Funzioni per la gestione dei pulsanti
+prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        showSlide(currentIndex);
+        updateButtons();
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        showSlide(currentIndex);
+        updateButtons();
+    }
+});
+
+// Inizializza l'array delle immagini
+for (let i = 0; i < images.length; i++) {
+    const img = document.createElement ('img');
+    img.src = images [i];
+    img.alt = `Image ${i + 1}`;
+    img.className = i === currentIndex ? 'visible' : 'hidden';
+    carouselSlide.appendChild(img);
+};
+
+
+
+
